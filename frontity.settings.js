700efaf2226ad7faea870928a9c606ff847a6e0b
Daniele Bertella <daniele.bertella@gmail.com>
2022-02-13 17:05:57 +0000

    Update settings to point to ildedalo

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,57 +1,46 @@
 const settings = {
-  "name": "il-dedalo",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
+  name: "il-dedalo",
+  state: {
+    frontity: {
+      url: "https://ildedalo.com",
+      title: "Il dedalo 🌸 Soul Space",
+      description:
+        "Il Dedalo è uno spazio per l'anima, la creatività, la cura e la bellezza di condividere.",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Chi Siamo", "/chi-siamo/"],
+            ["I Nostri Corsi", "/i-nostri-corsi/"],
+            ["Gli Spazi", "/gli-spazi/"],
+            ["Travel", "/category/travel/"],
+            ["Japan", "/tag/japan/"],
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: false,
+            showOnPost: true,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://test.frontity.org"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://ildedaloadmin.wordpress.com/",
+          postsPage: "eventi",
+          homepage: "home",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
