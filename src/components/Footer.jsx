3bdc9049f8174778f1c2faa3efd32e6950b16e1e
Daniele Bertella <daniele.bertella@gmail.com>
2025-07-06 21:53:21 +0100

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,38 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2024 Il Dedalo. Tutti i diritti riservati.</p>
+      <div className="footer-container">
+        <div className="footer-box">
+          <h4>Link Utili</h4>
+          <ul className="footer-list">
+            <li className="footer-list-item">
+              <a href="/la-tessera/">La Tessera</a>
+            </li>
+            <li className="footer-list-item">
+              <a href="/come-iscriversi/">Come Iscriversi ai Corsi</a>
+            </li>
+            <li className="footer-list-item">
+              <a href="/regolamento-corsi/">Regolamento Corsi</a>
+            </li>
+            <li className="footer-list-item">
+              <a href="/amministrazione-trasparente/">
+                Amministrazione Trasparente
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-box">
+          <h4>Dove Siamo</h4>
+          <span aria-label="love" style={{ marginRight: "0.5rem" }}>
+            🌿
+          </span>
+          Il Dedalo si trova a Casatenovo, in via Garibaldi 5
+          <li className="footer-list-item">
+            <a href="https://www.facebook.com/ildedalo">Facebook</a> ·{" "}
+            <a href="https://www.instagram.com/_ildedalo_/">Instagram</a> ·{" "}
+            <a href="https://goo.gl/maps/cKBwARWrqwyhCdYT7">Maps</a>
+          </li>
+        </div>
       </div>
     </footer>
   );
